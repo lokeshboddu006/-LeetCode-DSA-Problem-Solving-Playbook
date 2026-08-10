1bd01d6c2ce5aed1e5f8eb69b7e01ceca559d758
Lokesh Boddu <lokesh.boddu006@gmail.com>
2026-08-10 14:21:18 +0530

    Time: 14 ms (91.38%) | Memory: 42.2 MB (90.34%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] f = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k * k <= i; k++) {
+                if (!f[i - k * k]) {
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+        return f[n];
+    }
+}
