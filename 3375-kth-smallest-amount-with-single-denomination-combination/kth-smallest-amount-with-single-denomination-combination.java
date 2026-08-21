21ef79f3c0cb12726a425783607f4bf05f617ded
Lokesh Boddu <lokesh.boddu006@gmail.com>
2026-08-21 11:59:37 +0530

    Time: 176 ms (11.76%) | Memory: 43.2 MB (96.08%) - LeetSync

--- 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
@@ -1,48 +1,59 @@
 class Solution {
-    private int[] coins;
-    private int k;
-
     public long findKthSmallest(int[] coins, int k) {
-        this.coins = coins;
-        this.k = k;
-        long l = 1, r = (long) 1e11;
-        while(l < r) {
-            long mid = (l+r) >> 1;
-            if(check(mid)) {
-                r = mid;
+        long minCoin = coins[0];
+        for (int c : coins) {
+            minCoin = Math.min(minCoin, c);
+        }
+
+        long low = 1;
+        long high = minCoin * k;
+        long ans = high;
+
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            if (countMultiples(coins, mid) >= k) {
+                ans = mid;
+                high = mid - 1;
             } else {
-                l = mid+1;
+                low = mid + 1;
             }
         }
-        return l;
+        return ans;
     }
 
-    private boolean check(long mx) {
-        long cnt = 0;
+    private long countMultiples(int[] coins, long m) {
         int n = coins.length;
-        for(int i = 1; i < 1 << n; ++i) {
-            long v = 1;
-            for(int j = 0; j < n; ++j) {
-                if((i >> j & 1) == 1) {
-                    v = lcm(v, coins[j]);
-                    if(v > mx) break;
+        long count = 0;
+
+        // Inclusion-Exclusion Principle using bitmasks
+        for (int mask = 1; mask < (1 << n); mask++) {
+            long currentLcm = 1;
+            int bitCount = 0;
+
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    bitCount++;
+                    currentLcm = lcm(currentLcm, coins[i]);
+                    if (currentLcm > m) break; // Avoid overflow / unnecessary computation
                 }
             }
-            int m = Integer.bitCount(i);
-            if(m % 2 == 1) {
-                cnt += mx / v;
-            } else {
-                cnt -= mx / v;
+
+            if (currentLcm <= m) {
+                if (bitCount % 2 == 1) {
+                    count += m / currentLcm;
+                } else {
+                    count -= m / currentLcm;
+                }
             }
         }
-        return cnt >= k;
-    }
-
-    private long lcm(long a, long b) {
-        return a * b / gcd(a, b);
+        return count;
     }
 
     private long gcd(long a, long b) {
-        return b == 0 ? a : gcd(b, a%b);
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    private long lcm(long a, long b) {
+        return (a / gcd(a, b)) * b;
     }
 }
