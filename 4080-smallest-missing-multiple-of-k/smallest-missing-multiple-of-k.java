a071a7503cccd00cf59a5f6e98b7159494aceb83
Lokesh Boddu <lokesh.boddu006@gmail.com>
2026-08-25 14:37:18 +0530

    Time: 1 ms (82.00%) | Memory: 45.2 MB (66.75%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -1,14 +1,13 @@
 class Solution {
-
     public int missingMultiple(int[] nums, int k) {
-        boolean[] arr = new boolean[201];
-        for (int i : nums) {
-            arr[i] = true;
-        }
-        int ans = k;
-        while (arr[ans]) {
-            ans += k;
-        }
-        return ans;
+        long[] x = {0L, 0L};
+        for (int n : nums)
+            if (n % k == 0) {
+                int i = n / k - 1;
+                x[i >> 6] |= 1L << (i & 63);
+            }
+            
+        int z = x[0] == -1L ? 1 : 0;
+        return (z * 64 + Long.numberOfTrailingZeros(++x[z] & -x[z]) + 1) * k;
     }
 }
