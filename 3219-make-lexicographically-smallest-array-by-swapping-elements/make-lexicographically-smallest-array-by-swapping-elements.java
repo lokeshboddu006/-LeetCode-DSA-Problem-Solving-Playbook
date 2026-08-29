9044d76c03396bdee72f45109aaef2a8ca75d8f0
Lokesh Boddu <lokesh.boddu006@gmail.com>
2026-08-29 12:10:59 +0530

    Time: 72 ms (89.39%) | Memory: 146.8 MB (75.76%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,24 +1,25 @@
+import java.util.*;
+
 class Solution {
-    public int[] lexicographicallySmallestArray(int[] A, int limit) {
-         int[] srt = A.clone();
-        Arrays.sort(srt);
-        List<List<Integer>> grps = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        int id = -1;
-        for (int i = 0; i < srt.length; i++) {
-            if (i == 0 || srt[i] - srt[i - 1] > limit) {
-                grps.add(new ArrayList<>());
-                id++;
-            }    
-            grps.get(id).add(srt[i]);
-            map.put(srt[i], id);
+    public int[] lexicographicallySmallestArray(int[] a, int k) {
+        int n = a.length, r[] = new int[n];
+        Integer[] p = new Integer[n];
+        for (int i = 0; i < n; i++) p[i] = i;
+        
+        Arrays.sort(p, (i, j) -> Integer.compare(a[i], a[j]));
+
+        for (int i = 0; i < n; ) {
+            int j = i + 1;
+            while (j < n && a[p[j]] - a[p[j - 1]] <= k) j++;
+            
+            Integer[] x = Arrays.copyOfRange(p, i, j);
+            Arrays.sort(x);
+            
+            for (int t = 0; t < x.length; t++) {
+                r[x[t]] = a[p[i + t]];
+            }
+            i = j;
         }
-        int[] idx = new int[grps.size()];
-        for (int i = 0; i < A.length; i++) {
-            int cur = map.get(A[i]);
-            A[i] = grps.get(cur).get(idx[cur]);
-            idx[cur]++;
-        }
-        return A;
+        return r;
     }
 }
