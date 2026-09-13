ac8ce01d8da3c3e4446c4245b20012f2635067d8
Lokesh Boddu <lokesh.boddu006@gmail.com>
2026-09-13 08:26:45 +0530

    Time: 30 ms (95.83%) | Memory: 44.3 MB (85.23%) - LeetSync

--- 864-image-overlap/image-overlap.java
+++ 864-image-overlap/image-overlap.java
@@ -1,23 +1,29 @@
-import java.util.*;
-
 class Solution {
-    public int largestOverlap(int[][] a, int[][] b) {
-        int n = a.length, m = 0;
-        List<Integer> x = new ArrayList<>(), y = new ArrayList<>();
-        Map<Integer, Integer> c = new HashMap<>();
-        
-        for (int i = 0; i < n * n; i++) {
-            if (a[i / n][i % n] == 1) x.add((i / n) * 100 + (i % n));
-            if (b[i / n][i % n] == 1) y.add((i / n) * 100 + (i % n));
-        }
-        
-        for (int i : x) {
-            for (int j : y) {
-                int d = i - j;
-                c.put(d, c.getOrDefault(d, 0) + 1);
-                m = Math.max(m, c.get(d));
+    protected int shiftAndCount(int xShift, int yShift, int[][] M, int[][] R) {
+        int leftShiftCount = 0, rightShiftCount = 0;
+        int rRow = 0;
+        for (int mRow = yShift; mRow < M.length; ++mRow) {
+            int rCol = 0;
+            for (int mCol = xShift; mCol < M.length; ++mCol) {
+                if (M[mRow][mCol] == 1 && M[mRow][mCol] == R[rRow][rCol])
+                    leftShiftCount += 1;
+                if (M[mRow][rCol] == 1 && M[mRow][rCol] == R[rRow][mCol])
+                    rightShiftCount += 1;
+                rCol += 1;
             }
+            rRow += 1;
         }
-        return m;
+        return Math.max(leftShiftCount, rightShiftCount);
+    }
+    public int largestOverlap(int[][] A, int[][] B) {
+        int maxOverlaps = 0;
+
+        for (int yShift = 0; yShift < A.length; ++yShift)
+            for (int xShift = 0; xShift < A.length; ++xShift) {
+                maxOverlaps = Math.max(maxOverlaps, shiftAndCount(xShift, yShift, A, B));
+                maxOverlaps = Math.max(maxOverlaps, shiftAndCount(xShift, yShift, B, A));
+            }
+
+        return maxOverlaps;
     }
 }
