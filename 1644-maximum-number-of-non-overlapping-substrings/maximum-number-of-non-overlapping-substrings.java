e4b0e3eeeceaba2c42286f7ef74615ada09f72aa
Lokesh Boddu <lokesh.boddu006@gmail.com>
2026-09-18 22:41:13 +0530

    Time: 12 ms (44.76%) | Memory: 47.9 MB (88.24%) - LeetSync

--- 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.java
+++ 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.java
@@ -1,120 +1,32 @@
+import java.util.*;
+
 class Solution {
-    private int getRight(
-        String s,
-        int L,
-        int[] first,
-        int[] last
-    ) {
-
-        int R =
-            last[s.charAt(L) - 'a'];
-
-        for (int i = L; i <= R; i++) {
-
-            int c =
-                s.charAt(i) - 'a';
-
-            if (first[c] < L) {
-                return -1;
-            }
-
-            R = Math.max(
-                R,
-                last[c]
-            );
-        }
-
-        return R;
-    }
-
-    public List<String> maxNumOfSubstrings(
-        String s
-    ) {
-
+    public List<String> maxNumOfSubstrings(String s) {
+        int[] l = new int[26];
+        int[] r = new int[26];
+        Arrays.fill(l, -1);
         int n = s.length();
-
-        int[] first = new int[26];
-        int[] last = new int[26];
-
-        Arrays.fill(first, n);
-        Arrays.fill(last, -1);
-
-   
         for (int i = 0; i < n; i++) {
-
-            int c =
-                s.charAt(i) - 'a';
-
-            first[c] =
-                Math.min(first[c], i);
-
-            last[c] = i;
+            int c = s.charAt(i) - 'a';
+            if (l[c] == -1) l[c] = i;
+            r[c] = i;
         }
-
-        List<int[]> intervals =
-            new ArrayList<>();
-
-        for (int c = 0; c < 26; c++) {
-
-            if (first[c] == n) {
-                continue;
+        List<String> ans = new ArrayList<>();
+        int last = -1;
+        for (int i = 0; i < n; i++) {
+            if (i != l[s.charAt(i) - 'a']) continue;
+            int e = r[s.charAt(i) - 'a'];
+            int k = i;
+            while (k <= e && l[s.charAt(k) - 'a'] >= i) {
+                e = Math.max(e, r[s.charAt(k) - 'a']);
+                k++;
             }
-
-            int L = first[c];
-
-            int R =
-                getRight(
-                    s,
-                    L,
-                    first,
-                    last
-                );
-
-            if (R != -1) {
-                intervals.add(
-                    new int[]{L, R}
-                );
+            if (k > e) {
+                if (i <= last) ans.set(ans.size() - 1, s.substring(i, e + 1));
+                else ans.add(s.substring(i, e + 1));
+                last = e;
             }
         }
-
-        intervals.sort((a, b) -> {
-
-            if (a[1] != b[1]) {
-                return Integer.compare(
-                    a[1],
-                    b[1]
-                );
-            }
-
-            return Integer.compare(
-                b[0],
-                a[0]
-            );
-        });
-
-        List<String> ans =
-            new ArrayList<>();
-
-        int lastEnd = -1;
-
-        for (int[] interval : intervals) {
-
-            int L = interval[0];
-            int R = interval[1];
-
-            if (L > lastEnd) {
-
-                ans.add(
-                    s.substring(
-                        L,
-                        R + 1
-                    )
-                );
-
-                lastEnd = R;
-            }
-        }
-
         return ans;
     }
 }
